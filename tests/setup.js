4352487c7fb0efd5944e8e09abc36f4f69f75a57
Sean Stocker <sean.stocker14@gmail.com>
2023-08-25 11:44:01 +0100

    feat: added setup for jest tests
    Additions:
    - Created setup file for starting up a mongoose connection
    
    Modifications:
    - Modified package.json to use setup file when running jest

---
+++ tests/setup.js
@@ -0,0 +1,15 @@
+require("../models/User");
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+});
